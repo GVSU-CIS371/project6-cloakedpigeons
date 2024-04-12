7122e0a985d7d3b5319fac8d1d3df8bc968d004e
hobbsAlex <77682400+hobbsAlex@users.noreply.github.com>
2024-04-12 10:12:58 -0400

    copied code from project 5

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "./components/Home.vue";
+import GroceriesView from "./components/Groceries.vue";
+import ElectronicsView from "./components/Electronics.vue";
+import ClothingView from "./components/Clothing.vue";
+import BestSellerView from "./components/BestSeller.vue";
+
 const routes = [
   {
     path: "/",
@@ -8,6 +13,30 @@ const routes = [
     component: HomeView,
   },
   // you need to add more routes here
+  {
+    path: "/groceries",
+    name: "groceries",
+    props: true,
+    component: GroceriesView,
+  },
+  {
+    path: "/electronics",
+    name: "electronics",
+    props: true,
+    component: ElectronicsView,
+  },
+  {
+    path: "/clothing",
+    name: "clothing",
+    props: true,
+    component: ClothingView,
+  },
+  {
+    path: "/bestseller",
+    name: "bestseller",
+    props: true,
+    component: BestSellerView,
+  },
 ];
 
 const router = createRouter({
--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -2,6 +2,28 @@ import { defineStore } from "pinia";
 import { ProductDoc } from "../types/product";
 import { initProducts } from "../data-init";
 
+export let products = [] as ProductDoc[];
+
 export const useProductStore = defineStore("ProductStore", {
-  // your answer
+  // Define products state, initially empty array of ProductDoc
+  state: () => ({
+    return: { products },
+  }),
+  actions: {
+    // initialize the products with initProducts from data-init.ts
+    init() {
+      products=initProducts;
+    },
+    filterByCategory(category: string) {
+      products.filter((a) => a.data.category = category);
+    },
+    filterByRating(minRating: number) {
+      products.filter((a) => a.data.rating >= minRating)
+    },
+  },
+  getters: {
+    products(): ProductDoc[] {
+      return products; 
+    },
+  },
 });
