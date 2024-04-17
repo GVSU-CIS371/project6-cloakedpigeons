0773d98a2713b3485aaf382217582b4095206baa
hobbsAlex <77682400+hobbsAlex@users.noreply.github.com>
2024-04-17 16:46:27 -0400

    added option to create new items

--- src/components/newItem.vue
+++ src/components/newItem.vue
@@ -3,25 +3,29 @@
         <v-card class="justify-center">
           <div class="field">
             <label>Product Name:   </label>
-            <input type="text" />
+            <input type="text" id="productName" v-model="productName"/>
           </div>
           <div class="field">
             <label>Rating:   </label>
-            <input type="text" />
+            <input type="text" id="rating" v-model="rating"/>
           </div>
           <div class="field">
             <label>Price:   </label>
-            <input type="text" />
+            <input type="text" id="price" v-model="price"/>
           </div>
           <div class="field">
             <label>Stock:   </label>
-            <input type="text" />
+            <input type="text" id="stock" v-model="stock"/>
           </div>
           <div class="field">
             <label>Description:   </label>
-            <input type="text" />
+            <input type="text" id="description" v-model="description"/>
           </div>
-        <button>Add Item</button>
+          <div class="field">
+            <label>Category:   </label>
+            <input type="text" id="category" v-model="category"/>
+          </div>
+        <button @click="addItem">Add Item</button>
         <v-spacer></v-spacer>
         <button @click="goBack">Go Back</button>
         </v-card>
@@ -29,15 +33,53 @@
 </template>
 
 <script lang="ts" setup>
-  //import { defineProps, computed } from "vue";
+  import { defineProps, computed, ref } from "vue";
+  import { ProductDoc } from "../types/product";
   import { useRouter } from "vue-router";
   //import { randomUsers } from "../data/usersData";
   //import { product } from "../components/StoreItem.vue";
-  //const { id } = defineProps({
-  //  id: String,
-  //});
-  // const user = computed(() => randomUsers.value.find((u) => u.id === id));
+  import { app, db } from "../main"; // Import the firebase app
+  import { collection, addDoc, doc, setDoc, DocumentReference, getDocs, QueryDocumentSnapshot, QuerySnapshot } from "firebase/firestore";
+  
+  
+
+
+  //const user = computed(() => randomUsers.value.find((u) => u.id === id));
   const router = useRouter();
+  const productCollection = collection(db,"productStore");
+  const productName = ref('');
+  const description = ref('');
+  const price = ref(0);
+  const rating = ref(0);
+  const stock = ref(0);
+  const image = ref('');
+  const category = ref('');
+  let id ='';
+  getDocs(productCollection)
+      .then((querySnapshot) => {
+        id = (querySnapshot.size).toString();
+      });
+
+  function addItem() {
+    const productDoc = doc(db, "productStore", id); 
+      setDoc(productDoc, {
+      name: productName.value,
+
+      description: description.value,
+
+      price: price.value,
+
+      rating: rating.value,
+
+      stock: stock.value,
+
+      image: image.value,
+
+      category: category.value
+
+    });
+    router.back();
+  }
 
   function goBack() {
     router.back();
